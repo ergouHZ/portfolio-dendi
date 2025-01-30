90f5399ac1461ad868ab2033387fb1f810a45a81
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2025-01-30 15:18:40 +0000

    update

--- src/app/home/experience/page.tsx
+++ src/app/home/experience/page.tsx
@@ -1,9 +1,7 @@
 import EducationList from "@/components/EducationList/EducationList";
 
-
 export default function page() {
   return (
-    
     <EducationList />
   )
 }
--- src/app/projects/chat-search/page.tsx
+++ src/app/projects/chat-search/page.tsx
@@ -1,8 +1,7 @@
 import ChatLayout from "@/components/ChatSearchProject/ChatLayout/ChatLayout";
+
 export default function page() {
   return (
-    <div>
-        <ChatLayout />
-    </div>
+    <ChatLayout/>
   )
 }
